24fc79839a5b3f4de772a135df809e81fe4e75b4
Daiara Dyba <daiaradyba@gmail.com>
2021-09-15 18:34:09 -0300

    Adicionada plataforma

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var plataforma;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,6 +34,7 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    plataforma = new Ground(150,305,300,170);
     bird = new Bird(100,100);
 
 }
@@ -59,4 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    plataforma.display();
 }
